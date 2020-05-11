35d1b79d32a70686f67e43917f22732b74630f4e
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-12 03:53:53 +0530

    Welcome message display

---
+++ EmployeeWageViaMethods.java
@@ -0,0 +1,9 @@
+package com.employeeWageUsingMethods;
+
+class EmployeeWageViaMethods 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+}
