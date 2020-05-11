1dd9df5db5e0d592b61d925009a93ac359320067
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-12 04:05:54 +0530

    Daily wage computation and its display

--- EmployeeWageViaMethods.java
+++ EmployeeWageViaMethods.java
@@ -5,11 +5,17 @@ class EmployeeWageViaMethods
 	public static void main(String args[])
 	{	
 		final int isPresent=1;
+		int empDailyWage=0, empDailyHours=0, empWagePerHour=20;
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int empCheck=(int)(Math.floor(Math.random()*10)%2);
 		if(empCheck==isPresent)
-			System.out.println("The Employee is Present");
+		{	
+			System.out.println("Employee is Present");
+			empDailyHours=8;
+		}	
 		else
-			System.out.println("The Employee is Absent");
+			System.out.println("Employee is Absent");
+		empDailyWage=empDailyHours*empWagePerHour;
+		System.out.println("Today's Wage of Employee is :"+empDailyWage);
 	}
 }
