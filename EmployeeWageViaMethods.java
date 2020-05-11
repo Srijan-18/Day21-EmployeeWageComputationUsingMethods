cfe302907d44bd1c0b64521977f4c4c93f57a8e7
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-12 04:14:06 +0530

    Daily Wage computation using SWITCH CASE and its Display

--- EmployeeWageViaMethods.java
+++ EmployeeWageViaMethods.java
@@ -8,18 +8,19 @@ class EmployeeWageViaMethods
 		int empDailyWage=0, empDailyHours=0, empWagePerHour=20;
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int empCheck=(int)(Math.floor(Math.random()*10)%3);
-		if(empCheck==isFullTime)
-		{	
-			System.out.println("Employee is Present");
-			empDailyHours=8;
-		}	
-		else if(empCheck==isPartTime)
+		switch (empCheck)
 		{
-			System.out.println("Employee is present part time");
-			empDailyHours=4;
-		}
-		else
-			System.out.println("Employee is Absent");
+			case isFullTime :
+				System.out.println("Employee is Present");
+				empDailyHours=8;
+			break;
+			case isPartTime :
+				System.out.println("Employee is present part time");
+				empDailyHours=4;
+			break;
+			default :
+				System.out.println("Employee is Absent");
+		}	
 		empDailyWage=empDailyHours*empWagePerHour;
 		System.out.println("Today's Wage of Employee is :"+empDailyWage);
 	}
