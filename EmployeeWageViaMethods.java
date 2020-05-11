468e72895276074a4b809db049e8ca5023e9b582
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-12 04:19:54 +0530

    Monthly Wage calculation using and its Display

--- EmployeeWageViaMethods.java
+++ EmployeeWageViaMethods.java
@@ -3,26 +3,31 @@ package com.employeeWageUsingMethods;
 class EmployeeWageViaMethods 
 {
 	public static void main(String args[])
-	{	
-		final int isFullTime=1 ,isPartTime=2;
-		int empDailyWage=0, empDailyHours=0, empWagePerHour=20;
+	{	//welcome message
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int empCheck=(int)(Math.floor(Math.random()*10)%3);
-		switch (empCheck)
-		{
-			case isFullTime :
-				System.out.println("Employee is Present");
-				empDailyHours=8;
-			break;
-			case isPartTime :
-				System.out.println("Employee is present part time");
-				empDailyHours=4;
-			break;
-			default :
-				System.out.println("Employee is Absent");
-		}	
+		//constants
+		final int isFullTime=1 ,isPartTime=2;
+		//variables
+		int empDailyWage=0, empDailyHours=0, empWagePerHour=20;
+		int empMonthlyWage=0;
+		
+		//computation of Monthly wage with maximum working days in a month as 20
+		for (int i=1;i<=20;i++)
+		{	
+			int empCheck=(int)(Math.floor(Math.random()*10)%3);
+			switch (empCheck)
+			{
+				case isFullTime :
+					empDailyHours=8;
+				break;
+				case isPartTime :
+					empDailyHours=4;
+				break;
+				default :		
+					}	
 		empDailyWage=empDailyHours*empWagePerHour;
-		System.out.println("Today's Wage of Employee is :"+empDailyWage);
+		empMonthlyWage+=empDailyWage;
+		}
+		System.out.println("Monthly Wage of Employee is :"+empMonthlyWage);
 	}
-
 }
