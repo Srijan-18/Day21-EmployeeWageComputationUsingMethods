b35a9ca11103d9fee6c7f76c822e5a6034919075
Srijan-18 <srijan.mishrasir@gmail.com>
2020-05-12 04:02:59 +0530

    Employee attendance generated using random method and display

--- EmployeeWageViaMethods.java
+++ EmployeeWageViaMethods.java
@@ -3,7 +3,13 @@ package com.employeeWageUsingMethods;
 class EmployeeWageViaMethods 
 {
 	public static void main(String args[])
-	{
+	{	
+		final int isPresent=1;
 		System.out.println("Welcome to Employee Wage Computation Program");
+		int empCheck=(int)(Math.floor(Math.random()*10)%2);
+		if(empCheck==isPresent)
+			System.out.println("The Employee is Present");
+		else
+			System.out.println("The Employee is Absent");
 	}
 }
